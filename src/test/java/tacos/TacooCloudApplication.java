9e81a9dfb83e5bcec605a746b04f33fc826ecbd9
rus_yanov <ahmedzyanov.r.1997@gmail.com>
2023-11-12 21:45:31 +0300

    switch to maven project

--- src/test/java/tacos/TacooCloudApplication.java
+++ src/test/java/tacos/TacooCloudApplication.java
@@ -1,13 +1,13 @@
-package tacocloud;
+package tacos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TacoCloudApplicationTests {
+class TacooCloudApplication {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
